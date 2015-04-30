39995bdbde30ceb0ce4266317ff67bd5fa3e968f
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-04-30 16:13:45 +0100

    Committing schema change from migration
    This wasn't checked in the last time it was changed.

--- db/schema.rb
+++ db/schema.rb
@@ -504,7 +504,7 @@ ActiveRecord::Schema.define(version: 20150422083934) do
 
   create_table "feature_flags", force: true do |t|
     t.string  "key"
-    t.boolean "enabled"
+    t.boolean "enabled", default: false
   end
 
   create_table "feature_lists", force: true do |t|
@@ -597,8 +597,8 @@ ActiveRecord::Schema.define(version: 20150422083934) do
     t.string   "name"
     t.date     "start_date"
     t.date     "end_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "governments", ["end_date"], name: "index_governments_on_end_date", using: :btree
